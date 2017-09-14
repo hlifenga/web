b4ff64a7deaab11a9ba60fc3a325e22aee192b3b
rufus <hlifenga@gmail.com>
2017-09-14 13:42:50 +0800

    inintial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.get('/',function(request, response){
+    response.send('hello world!');
+});
+
+app.listen(port,function(){
+    console.log("Express app listening on port " + port);
